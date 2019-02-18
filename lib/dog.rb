45cf46c3fc412d5b849195d1f50e9f861c6c0e37
Kylla Harris <kylla.harris@gmail.com>
2019-02-18 20:17:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,9 @@
 class Dog
-  def initialize(name)
-    @name = name
+  def name 
+    @name 
   end 
-  def name
-    @name
-  end
-  def name=(new_name)
-    @name = new_name
+  
+  def name=(name)
+    @name = name
   end
 end
